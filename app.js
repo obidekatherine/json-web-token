9b7d7bd463e3d1d2a61a5f8e8a43b0f422b8825d
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2022-05-06 14:29:30 +0100

    update

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-require('dotenv').config()
-require('express-async-errors')
+require('dotenv').config();
+require('express-async-errors');
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
+const notFoundMiddleware = require('./main/middleware/not-found');
+const errorHandlerMiddleware = require('./main/middleware/error-handler');
 
-const notFoundMiddleware = require('./main/middleware/not-found')
-const errorHandleMiddleware = require('./main/middleware/error-handler')
+// middleware
+app.use(express.static('./public'));
+app.use(express.json());
 
-
-//middleware
-app.use(notFoundMiddleware)
-app.use(errorHandleMiddleware)
-app.use(express.static('./public'))
-
-
-//express middleware
-app.use(express.json())// one of the route is a post route and i want to access req.body
-
-
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 4000;
 
-const start = async ()=> {
-try {
-    app.listen(port, console.log(`Server is listening on port ${port}...`))
-} catch (error) {
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
     console.log(error);
-}
-}
+  }
+};
 
-start()
+start();
